ae1b644207334ab2a8db771088ca7c360e38f40a
ZhyrexCalamiong <xeryhzcalamiong@gmail.com>
2024-10-18 20:32:25 +0800

    test for hosting

--- back_end/index.js
+++ back_end/index.js
@@ -4,9 +4,12 @@ import router from './api/routes.js';
 import budget from './api/budgetroutes.js';
 import convert from './api/moneyconvert.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.Port || 8000
 
 app.use(cors());
 app.use(express.json());
