e75c5cab1ce96a0f0976bd614514858173e8b0f3
Dal Ramos <d4rukun@gmail.com>
2024-10-18 10:25:41 +0800

    Scaled Home PAge

--- front_end/lib/features/home/pages/home_page.dart
+++ front_end/lib/features/home/pages/home_page.dart
@@ -47,85 +47,82 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(
           color: const Color(0xFF0D0D0D),
           child: Center(
-            // Use Center widget for centering content
             child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Center the Column
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 30), // Reduced space
+                  const SizedBox(height: 40),
                   const Text(
                     'Welcome to BudgetWise',
                     style: TextStyle(
-                      color: Color(0xFF8BBE6D), // Updated highlight color
-                      fontSize: 24, // Reduced font size
+                      color: Color(0xFF8BBE6D),
+                      fontSize: 26,
                       fontWeight: FontWeight.bold,
                     ),
+                    textAlign: TextAlign.center,
                   ),
-                  const SizedBox(height: 12), // Reduced space
+                  const SizedBox(height: 16),
                   const Text(
                     'Your all-in-one solution for personal finance management',
                     style: TextStyle(
-                        color: Colors.white, fontSize: 14), // Reduced font size
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                     textAlign: TextAlign.center,
                   ),
-                  const SizedBox(height: 20), // Reduced space
-                  Padding(
-                    padding: const EdgeInsets.all(8.0), // Reduced padding
-                    child: Column(
-                      children: [
-                        _featureCard(
-                          title: 'Currency Converter',
-                          icon: Icons.currency_exchange,
-                          description:
-                              'Easily convert currencies with real-time exchange rates.',
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const CurrencyConverterScreen(),
-                              ),
-                            );
-                          },
+                  const SizedBox(height: 24),
+                  _featureCard(
+                    title: 'Currency Converter',
+                    icon: Icons.currency_exchange,
+                    description:
+                        'Easily convert currencies with real-time exchange rates.',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CurrencyConverterScreen(),
                         ),
-                        _featureCard(
-                          title: 'Analytics',
-                          icon: Icons.bar_chart,
-                          description:
-                              'Get insights into your spending and saving habits.',
-                          onTap: () {
-                            setState(() {
-                              _selectedIndex = 1;
-                            });
-                          },
-                        ),
-                        _featureCard(
-                          title: 'Wallet',
-                          icon: Icons.account_balance_wallet,
-                          description:
-                              'Manage your funds and track expenses easily.',
-                          onTap: () {
-                            setState(() {
-                              _selectedIndex = 2;
-                            });
-                          },
-                        ),
-                        _featureCard(
-                          title: 'Profile',
-                          icon: Icons.person,
-                          description:
-                              'Update your personal information and preferences.',
-                          onTap: () {
-                            setState(() {
-                              _selectedIndex = 3;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
+                  const SizedBox(height: 12),
+                  _featureCard(
+                    title: 'Analytics',
+                    icon: Icons.bar_chart,
+                    description:
+                        'Get insights into your spending and saving habits.',
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  _featureCard(
+                    title: 'Wallet',
+                    icon: Icons.account_balance_wallet,
+                    description: 'Manage your funds and track expenses easily.',
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  _featureCard(
+                    title: 'Profile',
+                    icon: Icons.person,
+                    description:
+                        'Update your personal information and preferences.',
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = 3;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 24),
                 ],
               ),
             ),
@@ -153,34 +150,38 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         elevation: 4,
         child: Padding(
-          padding: const EdgeInsets.all(8.0), // Further reduced padding
+          padding: const EdgeInsets.all(16.0),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Icon(
                 icon,
-                color: const Color(0xFF8BBE6D), // Updated highlight color
-                size: 30, // Reduced icon size
+                color: const Color(0xFF8BBE6D),
+                size: 32,
               ),
-              const SizedBox(width: 8), // Reduced space
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      color: Color(0xFF8BBE6D), // Updated highlight color
-                      fontSize: 14, // Reduced font size
-                      fontWeight: FontWeight.bold,
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Color(0xFF8BBE6D),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 2), // Reduced space
-                  Text(
-                    description,
-                    style: const TextStyle(
+                    const SizedBox(height: 8),
+                    Text(
+                      description,
+                      style: const TextStyle(
                         color: Colors.white70,
-                        fontSize: 12), // Reduced font size
-                  ),
-                ],
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
